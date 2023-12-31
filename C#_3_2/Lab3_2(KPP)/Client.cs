2505022bae1edde5e9cfbabbe1f2dbe3680d0e40
Oleh Mykhalskyi <xukaro11@gmail.com>
2023-12-31 17:05:58 +0200

    C# lab 3.2

---
+++ C#_3_2/Lab3_2(KPP)/Client.cs
@@ -0,0 +1,94 @@
+﻿using SimpleTCP;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab3_2_KPP_
+{
+    public partial class Client : Form
+    {
+        public Client()
+        {
+            InitializeComponent();
+        }
+
+        SimpleTcpClient client;
+
+        private void Client_Load(object sender, EventArgs e)
+        {
+            client = new SimpleTcpClient();
+            client.StringEncoder = Encoding.UTF8;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.Enabled = false;
+            client.Connect(textBox1.Text, Convert.ToInt32(textBox2.Text));
+        }
+
+        private void query()
+        {
+            string query = "0, ";
+
+            if (checkBox1.Checked == true)
+            {
+                query += "1, ";
+            }
+            if (checkBox2.Checked == false)
+            {
+                query += "2, ";
+            }
+
+            if (radioButton1.Checked == true)
+            {
+                query += "3, ";
+            }
+            else if (radioButton2.Checked == true)
+            {
+                query += "4, ";
+            }
+            else if(radioButton3.Checked == true)
+            {
+                query += "5, ";
+            }
+
+            query += "0";
+            textBox3.Text = query;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            query();
+            client.WriteLineAndGetReply(textBox3.Text, TimeSpan.FromSeconds(1));
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            query();
+            client.WriteLineAndGetReply(textBox3.Text, TimeSpan.FromSeconds(1));
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            query();
+            client.WriteLineAndGetReply(textBox3.Text, TimeSpan.FromMilliseconds(1));
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            query();
+            client.WriteLineAndGetReply(textBox3.Text, TimeSpan.FromMilliseconds(1));
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            query();
+            client.WriteLineAndGetReply(textBox3.Text, TimeSpan.FromMilliseconds(1));
+        }
+    }
+}
---
+++ C#_3_2/Server/Server.cs
@@ -0,0 +1,121 @@
+﻿using SimpleTCP;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Server
+{
+    public partial class Server : Form
+    {
+        public Server()
+        {
+            InitializeComponent();
+        }
+
+        SimpleTcpServer server;
+
+        private void Server_Load(object sender, EventArgs e)
+        {
+            server = new SimpleTcpServer();
+            server.Delimiter = 0x13; //Enter
+            server.StringEncoder = Encoding.UTF8;
+            server.DataReceived += Server_DataRecieved;
+        }
+
+        private void Server_DataRecieved(object sender, SimpleTCP.Message e)
+        {
+            string request = e.MessageString;
+            string[] keys = request.Split(',');
+
+            int iterator = 0;
+
+            bool checkbox1 = false;
+            bool checkbox2 = false;
+
+            while (iterator < keys.Length)
+            {
+                string numbers = Regex.Replace(keys[iterator], "[^0-9.+-]", "");
+
+                if (checkbox1 == false)
+                {
+                    this.Invoke((MethodInvoker)delegate ()
+                    {
+                        button3.Enabled = true;
+                    });
+                }
+                
+                if(checkbox2 == false)
+                {
+                    button3.Invoke((MethodInvoker)delegate ()
+                    {
+                        label3.Visible = true;
+                        panel1.Visible = true;
+                    });
+                }
+
+                if (Convert.ToInt32(numbers) == 1)
+                {
+                    this.Invoke((MethodInvoker)delegate ()
+                    {
+                        button3.Enabled = false;
+                        checkbox1 = true;
+                    });
+                }
+                else if (Convert.ToInt32(numbers) == 2)
+                {
+                    button3.Invoke((MethodInvoker)delegate ()
+                    {
+                        label3.Visible = false;
+                        panel1.Visible = false;
+                        checkbox2 = true;
+                    });
+                }
+                else if (Convert.ToInt32(numbers) == 3)
+                {
+                    this.Invoke((MethodInvoker)delegate ()
+                    {
+                        this.BackColor = SystemColors.Control;
+                    });
+                }
+                else if (Convert.ToInt32(numbers) == 4)
+                {
+                    this.Invoke((MethodInvoker)delegate ()
+                    {
+                        this.BackColor = Color.Red;
+                    });
+                }
+                else if (Convert.ToInt32(numbers) == 5)
+                {
+                    this.Invoke((MethodInvoker)delegate ()
+                    {
+                        this.BackColor = Color.Yellow;
+                    });
+                }
+
+                iterator++;
+            }
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            System.Net.IPAddress ip = System.Net.IPAddress.Parse(textBox1.Text);
+            server.Start(ip, Convert.ToInt32(textBox2.Text));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (server.IsStarted)
+            {
+                server.Stop();
+            }
+        }
+    }
+}
